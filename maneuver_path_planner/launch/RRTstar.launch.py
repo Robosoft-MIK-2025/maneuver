98783ac776fe8bcb852f48377a68ad29ca60bb0f
Evgenii Shlomov <e.shlomov@innopolis.university>
2025-09-22 00:30:54 +0300

    feat: maf

--- maneuver_path_planner/launch/RRTstar.launch.py
+++ maneuver_path_planner/launch/RRTstar.launch.py
@@ -5,149 +5,63 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-from launch_ros.descriptions import ComposableNode
-from launch_ros.actions import ComposableNodeContainer
-from moveit_configs_utils import MoveItConfigsBuilder
-
-
-
-
-
 
 def generate_launch_description():
-    
-
-    global_planner_param = os.path.join(
-        get_package_share_directory('maneuver_path_planner'),
-        'config',
-        'path_planer.yaml'
-    )
-
-    local_planner_param = os.path.join(
-        get_package_share_directory('maneuver_path_planner'),
-        'config',
-        'local_planer_params.yaml'
-    )
-
-    hybrid_planner_param = os.path.join(
-        get_package_share_directory('maneuver_path_planner'),
-        'config',
-        'hybrid_planner_params.yaml'
-    )
 
     urdf_path = os.path.join(
         get_package_share_directory('maneuver_bringup'),
-        'urdf', 
+        'urdf',
         'drone.urdf'
     )
     with open(urdf_path, 'r') as infp:
         robot_desc = infp.read()
     robot_description = {'robot_description': robot_desc}
-    
-     # SRDF — замените на путь к вашему SRDF, сгенерированному через MoveIt Setup Assistant
+
     srdf_path = os.path.join(
-        get_package_share_directory('drone_moveit_config'),  # пакет, где хранится SRDF
+        get_package_share_directory('drone_moveit_config'),
         'config',
         'drone.srdf'
     )
     with open(srdf_path, 'r') as infp:
         robot_desc_semantic = infp.read()
     robot_description_semantic = {'robot_description_semantic': robot_desc_semantic}
-    
-    # Здесь добавляем kinematics.yaml
+
     kinematics_path = os.path.join(
         get_package_share_directory('drone_moveit_config'),
         'config',
         'kinematics.yaml'
     )
-    # Загружаем как параметр
-    kinematics_parameters = {'kinematics_yaml': kinematics_path}
+
+    ompl_planning = os.path.join(
+        get_package_share_directory('drone_moveit_config'),
+        'config',
+        'ompl_planning.yaml'
+    )
+
+    moveit_controllers = os.path.join(
+        get_package_share_directory('drone_moveit_config'),
+        'config',
+        'moveit_controllers.yaml'
+    )
+
     rviz_config_path = os.path.join(
         get_package_share_directory('maneuver_path_planner'),
         'rviz',
         'moveit_config.rviz'
     )
 
-
-    # OMPL planner pipeline
-    # ompl_params = os.path.join(
-    # get_package_share_directory('maneuver_path_planner'),
-    # 'config',
-    # 'ompl_planning.yaml'
-    # )
-    # ompl_planning_pipeline_config = {'ompl_planning_pipeline_config': ompl_params}
-
-    ompl_planning_path = os.path.join(
-        get_package_share_directory('drone_moveit_config'))
-    ompl_planning = os.path.join(ompl_planning_path, 'config', 'ompl_planning.yaml')
-    planner_parameters = {'ros_parameters': ompl_planning}
-
-
-
-    controllers_path = os.path.join(
-        get_package_share_directory('drone_moveit_config'))
-    moveit_controllers = os.path.join(controllers_path, 'config', 'moveit_controllers.yaml')
-
-
-    # container = ComposableNodeContainer(
-    #         name="hybrid_planning_container",
-    #         namespace="/",
-    #         package="rclcpp_components",
-    #         executable="component_container",
-    #         composable_node_descriptions=[
-    #             ComposableNode(
-    #                 package="moveit_hybrid_planning",
-    #                 plugin="moveit::hybrid_planning::GlobalPlannerComponent",
-    #                 name="global_planner",
-    #                 parameters=[
-    #                     global_planner_param,
-    #                     robot_description,
-    #                     robot_description_semantic,
-    #                     kinematics_parameters,
-    #                     controllers_yaml,
-    #                     # ompl_planning_pipeline_config,
-    #                 ],
-    #             ),
-    #             ComposableNode(
-    #                 package="moveit_hybrid_planning",
-    #                 plugin="moveit::hybrid_planning::LocalPlannerComponent",
-    #                 name="local_planner",
-    #                 parameters=[
-    #                     local_planner_param,
-    #                     robot_description,
-    #                     robot_description_semantic,
-    #                     kinematics_parameters,
-    #                     controllers_yaml,
-    #                 ],
-    #             ),
-    #             ComposableNode(
-    #                 package="moveit_hybrid_planning",
-    #                 plugin="moveit::hybrid_planning::HybridPlanningManager",
-    #                 name="hybrid_planning_manager",
-    #                 parameters=[hybrid_planner_param],
-    #             ),
-    #         ],
-    #         output="screen",
-    # )
-
-
-    
+    sensors_yaml = os.path.join(
+    get_package_share_directory('drone_moveit_config'),
+    'config',
+    'sensors.yaml'
+    )
 
     return LaunchDescription([
-        # Node(
-        #     package='moveit_simple_controller_manager',
-        #     executable='ros2_control_node',
-        #     parameters=[controllers_yaml],
-        #     output='screen'
-        # ),
-
-
-        # container,
         Node(
             package="joint_state_publisher",
-            executable='joint_state_publisher',
-            name='joint_state_publisher',
-            output='screen',
+            executable="joint_state_publisher",
+            name="joint_state_publisher",
+            output="screen",
             parameters=[robot_description]
         ),
 
@@ -158,54 +72,63 @@ def generate_launch_description():
             output="screen",
             parameters=[
                 robot_description,
-                # {'planning_pipeline': 'ompl'},
                 robot_description_semantic,
-                {'planning_pipeline/planning_plugin': 'ompl_interface/OMPLPlanner'}
-                # # global_planner_param,
-                # # moveit_controllers,
-                # planner_parameters,
-                # kinematics_parameters
+                ompl_planning,         
+                kinematics_path,         
+                moveit_controllers,
+                sensors_yaml,   
+                {
+                    "planning_pipelines": ["ompl"],
+                    "default_planning_pipeline": "ompl",
+                    "ompl": {
+                        "planning_plugin": "ompl_interface/OMPLPlanner",
+                        "request_adapters": "default_planner_request_adapters/AddTimeOptimalParameterization "
+                                            "default_planner_request_adapters/FixWorkspaceBounds "
+                                            "default_planner_request_adapters/FixStartStateBounds "
+                                            "default_planner_request_adapters/FixStartStateCollision "
+                                            "default_planner_request_adapters/FixStartStatePathConstraints",
+                        "start_state_max_bounds_error": 0.1
+                    },
+                    "moveit_controller_manager": "moveit_simple_controller_manager/MoveItSimpleControllerManager",
+                    "moveit_simple_controller_manager": {
+                        "controller_names": ["drone_controller"]
+                    }
+                }
             ],
         ),
 
-        
-
         Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            output='screen',
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            name="robot_state_publisher",
+            output="screen",
             parameters=[robot_description]
         ),
-        
-        Node(
-	    package='tf2_ros',
-	    executable='static_transform_publisher',
-	    arguments=['0', '0', '0', '0', '0', '0', 'map', 'base_link'],
-	    name='static_tf_map_to_base'
-	    ),
 
-        
         Node(
-	    package="rviz2",
-	    executable="rviz2",
-	    name="rviz2",
-	    output="screen",
-	    arguments=["-d", rviz_config_path],
-	    parameters=[robot_description, robot_description_semantic]
-	    ),
-	
-	    Node(
-            package='octomap_server',
-            executable='octomap_server_node',
-            name='octomap_server',
-            output='screen',
+            package="tf2_ros",
+            executable="static_transform_publisher",
+            arguments=['0', '0', '0', '0', '0', '0', 'map', 'base_link'],
+            name="static_tf_map_to_base"
+        ),
+
+        Node(
+            package="rviz2",
+            executable="rviz2",
+            name="rviz2",
+            output="screen",
+            arguments=["-d", rviz_config_path],
+            parameters=[robot_description, robot_description_semantic]
+        ),
+
+        Node(
+            package="octomap_server",
+            executable="octomap_server_node",
+            name="octomap_server",
+            output="screen",
             parameters=[{
                 'frame_id': 'map',
-                'resolution': 0.05  # Можно изменить под ваши нужды
+                'resolution': 0.05
             }]
-  )
-        
-
-
+        )
     ])
